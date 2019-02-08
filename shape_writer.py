e0278505ae4dc371ecb167fb51bbd77d10bf8e10
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-02-08 20:42:23 -0200

    connects circles and squares (horizontally)

--- shape_writer.py
+++ shape_writer.py
@@ -2,7 +2,7 @@ i_m = 0.1 # inter module
 
 size('A3')
 
-p_shape = 0.5
+p_shape = 0.4
 
 _w = 10
 _h = 14
@@ -22,13 +22,26 @@ def d_square(x,y):
     rect(x * w + inter_column, y * h + inter_column, actual_width, actual_width)
 
 
+path = u'orange-horizontal.pdf'
+img_w, img_h = imageSize(path)
+
+
+scaling_x = (w * (1+p_shape/2))/img_w
+scaling_y = w*p_shape / img_h
 
 for x in range(_w):
     for y in range(_h):
         square = False
         if (y % 2 == x % 2 == 0) or (y % 2 == x % 2 == 1):
             square = True
-                
+
+        if x < _w - 1:
+            with savedState():
+                scale(x=scaling_x, y=scaling_y)
+                s_x = (x * w + i_m * w) / scaling_x
+                s_y = (y * h + i_m * w) / scaling_y
+                image(path, (s_x, s_y))
+                            
         if square:
             d_square(x,y)
         else:
